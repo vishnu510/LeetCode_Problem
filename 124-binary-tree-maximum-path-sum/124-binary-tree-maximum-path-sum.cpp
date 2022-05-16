c7ccb554becb7cdc4784ff2df0377e4d61b01639
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-05-17 02:12:37 +0530

    Time: 36 ms (46.60%), Space: 28.8 MB (6.30%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -12,29 +12,23 @@
 class Solution {
 public:
        int maxSum(TreeNode* root, int& ans) {
-    /* This function return the Branch Sum......
-    So if the node is NULL then it won't have a branch....so the branch sum will be 0.
-    */
-    //Base Case
+   
     if(root == NULL){
         return 0;
     }
-    
-    //Recursive Case 
-    //BS = Branch Sum
+   
     int leftBS = root->val + maxSum( root->left , ans );
     int rightBS = root->val + maxSum( root->right , ans );
     
     ans = max({
-                ans,            //we may have found the maximum ans already
-                root->val,      //may be the current root val is the maximum sum possible
-                leftBS,         //may be the answer contain root->val + left branch value
-                rightBS,        //may be the answer contain root->val + right branch value
-                leftBS + rightBS - root->val   // may be ans conatin left branch + right branch + root->val
-                                               // Since the root val is added twice from leftBS and rightBS so we are sunstracting it.
+                ans,           
+                root->val,      
+                leftBS,        
+                rightBS,        
+                leftBS + rightBS - root->val   
             });
     
-    //Return the max branch Sum
+   
     return max({ leftBS , rightBS , root->val });
 }
 
