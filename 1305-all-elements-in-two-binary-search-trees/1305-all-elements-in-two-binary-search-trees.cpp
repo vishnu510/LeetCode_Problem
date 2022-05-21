4c467e6e04d83ddece585624a80f6a5f6ab2d8a8
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-05-22 01:08:16 +0530

    Time: 206 ms (39.46%), Space: 86.2 MB (35.27%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,18 +11,38 @@
  */
 class Solution {
 public:
-    vector<int> result;
-    void elements(TreeNode* root){
-        if(!root) return;
-        result.push_back(root->val);
-        elements(root->left);
-        elements(root->right);
+   
+    void inorder(TreeNode* root,vector<int>&vec){
+        if(root)
+        {
+        inorder(root->left,vec);
+        vec.push_back(root->val);
+        inorder(root->right,vec);
+        }
+    }
+    void mergetrees(vector<int> v1,vector<int>v2,vector<int>&mergedtree){
+        int i=0,j=0,k=0;
+        while(i<v1.size() and j<v2.size()){
+            if(v1[i]<v2[j]){
+                mergedtree[k++] = v1[i++];
+            }
+            else{
+                mergedtree[k++] = v2[j++];
+            }
+        }
+        while(i<v1.size()){
+            mergedtree[k++] = v1[i++];
+        }
+        while(j<v2.size()){
+            mergedtree[k++] = v2[j++];
+        }
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        if(!(root1 || root2)) return {};
-        elements(root1);
-        elements(root2);
-        sort(result.begin(),result.end());
-        return result;
+        vector<int> tree1,tree2;
+        inorder(root1,tree1);
+        inorder(root2,tree2);
+        vector<int> mergedtree(tree1.size()+tree2.size());
+        mergetrees(tree1,tree2,mergedtree);
+        return mergedtree;
     }
 };
