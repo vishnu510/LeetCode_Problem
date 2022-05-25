723d78f03bc73d3e51786a41afff4f6ffabeac0c
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-05-26 01:29:45 +0530

    Time: 12 ms (78.96%), Space: 8.4 MB (76.47%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     string frequencySort(string s) {
+      priority_queue<pair<int,char>> MaxHeap;
         unordered_map<char,int> map;
-        priority_queue<pair<int,char>> Maxheap;
-        for(auto i:s){
-            map[i]++;
+        for(int i=0;i<s.size();i++)
+            map[s[i]]++;
+        for(auto i:map)
+            MaxHeap.push(make_pair(i.second,i.first));
+        string result="";
+        while(!MaxHeap.empty())
+        {
+            int freq=MaxHeap.top().first;
+            char str= MaxHeap.top().second;
+            for(int i=0;i<freq;i++)
+            {
+                result+=str;
+            }
+            MaxHeap.pop();
         }
-        string res;
-        for(auto j:map){
-            Maxheap.push({j.second,j.first});
-        }
-        while(!Maxheap.empty()){
-            res+=string(Maxheap.top().first,Maxheap.top().second);
-            Maxheap.pop();
-        }
-        return res;
+        return result;
     }
 };
