774a132dc0e6b6af5bc51975790bfe8e597636f4
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-05-28 05:18:50 +0530

    Time: 26 ms (57.41%), Space: 17 MB (22.72%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> map;
-        for(auto i:nums){
-            map[i]++;
+        int result = 0;
+        for(auto i:nums){   // Using XOR operation
+            result^=i;
         }
-        for(auto i=map.begin();i!=map.end();i++){
-            if(i->second==1){
-                return i->first;
-            }
-        }
-        return 0;
+        return result;
     }
 };
