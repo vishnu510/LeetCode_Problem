6857080759182b78d348219ba3a0be6076bc37d0
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-05-31 00:08:54 +0530

    Time: 12 ms (35.02%), Space: 6.8 MB (91.82%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -3,17 +3,13 @@ public:
     bool isIsomorphic(string s, string t) {
        int len = s.size();
         char seen[128] = {};
-        for (int i = 0; i < len; i++) {
-            // checking s, char by char
+        for(int i=0;i<len;i++){
             char c = s[i];
-            if (!seen[c]) {
-                // if we never saw this, we check it does not match any other seen
-                for (char s: seen) if (s == t[i]) return false;
-          
+            if(!seen[c]){
+                for(char str:seen) if(str==t[i]) return false;
                 seen[c] = t[i];
             }
-            
-            else if (seen[c] != t[i]) return false;
+            else if(seen[c]!=t[i]) return false;
         }
         return true;
     }
