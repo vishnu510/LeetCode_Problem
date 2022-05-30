a44e92b4c9769ab8b1918bb75b6175ea519db34f
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-05-30 23:46:27 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (77.75%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res;
+        while(columnNumber--){
+            res = (char)(columnNumber%26 + 'A') + res;
+            columnNumber/=26;
+        }
+        return res;
+    }
+};
