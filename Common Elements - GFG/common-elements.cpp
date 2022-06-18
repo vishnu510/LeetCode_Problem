237e21563cc6d1ef8f36f45747a7089e18b4aea3
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-06-18 09:54:55 +0530

    Added solution - LeetHub

---
+++ Common Elements - GFG/common-elements.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution{
+    public:
+        vector<int> common_element(vector<int>v1,vector<int>v2)
+    {
+        // Your code here
+        unordered_map<int,int> m1;
+        for(auto i:v2){
+            m1[i]++;
+        }
+        vector<int> ans;
+        for(auto i:v1){
+            if(m1[i]>0){
+                ans.push_back(i);
+                m1[i]--;
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        vector<int>v1(n,0);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v1[i];
+        }
+        int m;
+        cin>>m;
+        vector<int>v2(m,0);
+        for(int i=0;i<m;i++)
+        {
+            cin>>v2[i];
+        }
+        Solution ob;
+        vector<int>result;
+        result=ob.common_element(v1,v2);
+        for(auto i:result)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        
+    }
+}  // } Driver Code Ends
