d3cc1d5fbba655623eb6149d20d318378cc50926
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-06-24 19:32:05 +0530

    Added solution - LeetHub

--- Rearrange Array Alternately - GFG/rearrange-array-alternately.cpp
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.cpp
@@ -17,19 +17,24 @@ class Solution{
     { 
     	
     	// Your code here
-    	int left = 0;
-    	int right = n-1;
-    	vector<int> ans;
-    	while(left<right and n>1){
-    	    ans.push_back(arr[right--]);
-    	    ans.push_back(arr[left++]);
-    	}
-    	 if(n%2==1){
-    	     ans.push_back(arr[right]);
-    	 }
-    	 for(int i=0;i<n;i++){
-    	     arr[i] = ans[i];
-    	 }
+    	int maxIdx = n-1;
+    int minIdx = 0;
+    int maxElem = arr[maxIdx] + 1; // store any element that is greater than the maximum element in the array 
+    for( int i = 0; i < n; i++) {
+      // at even indices we will store maximum elements
+      if (i % 2 == 0){  
+        arr[i] += (arr[maxIdx] % maxElem ) * maxElem;
+        maxIdx -= 1;
+      }
+      else { // at odd indices we will store minimum elements
+        arr[i] += (arr[minIdx] % maxElem ) * maxElem;
+        minIdx += 1;
+      }
+    }
+    // dividing with maxElem to get original values.
+    for( int i = 0; i < n; i++) {
+      arr[i] = arr[i] / maxElem;
+    }
     }
 };
 
