95beef1f6cd3402f372c5d04afb93a3fe3a2eb0a
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-07-19 00:41:06 +0530

    Time: 54 ms (13.68%), Space: 15.4 MB (15.40%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,16 +1,59 @@
 class Solution {
 public:
+    
+    class Trie{
+        public:
+        
+        unordered_map<char,Trie*> children;
+        bool isEnd  = false;
+        
+        void insert(string word){
+            
+            Trie* node = this;
+            
+            for(char c:word){
+                
+                if(node->children.find(c)==node->children.end()){
+                    node->children[c] = new Trie();
+                }
+                
+                node = node->children[c];
+            }
+            
+            node->isEnd = true;
+        }
+        
+        bool search(string word){
+            
+            Trie* node = this;
+            for(char c:word){
+                if(node->children.find(c)==node->children.end()){
+                return false;
+            }
+                node = node->children[c];
+            }
+            return node->isEnd;
+        }
+        
+    };
+    
     bool wordBreak(string s, vector<string>& wordDict) {
+        
+        Trie trie;
+        
+        for(auto word:wordDict){
+            trie.insert(word);
+        }
          vector<bool> dp(s.size()+1,false);
         dp[0] = true;
         for(int i=1;i<=s.size();i++){
             for(int j=0;j<i;j++){
-               if((dp[j]) && (find(wordDict.begin(),wordDict.end(),s.substr(j,i-j))!=wordDict.end())){
-                   dp[i] = true;
-                   break;
-               }
+                
+                if(dp[j] && trie.search(s.substr(j,i-j))){
+                    dp[i] = true;
+                }
             }
         }
-        return dp.back();
+        return dp[s.size()];
     }
 };
