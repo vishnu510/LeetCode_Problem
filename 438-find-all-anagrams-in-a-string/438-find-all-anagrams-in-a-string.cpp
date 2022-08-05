7952f2221bff6c3435731fd554b7bc2f901c2852
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-08-05 15:48:33 +0530

    Time: 25 ms (54.45%), Space: 8.7 MB (39.03%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int lenofs = s.length(),lenofp = p.length();
-        if(lenofp>lenofs) return {};
-        vector<int> vecs(26,0),vecp(26,0),res;
-        for(int i=0;i<lenofp;i++){
-            vecp[p[i]-'a']++;
+         
+        int m = s.size() , n = p.size();
+        
+        vector<int> m1(26,0),m2(26,0);
+        
+        for(int i=0;i<n;i++){
+            m1[p[i]-'a']++;
         }
         int i=0,j=0;
-        while(j<lenofs){
-            vecs[s[j]-'a']++;
-            if((j-i+1)<lenofp){
+        vector<int> ans;
+        while(j<m){
+            
+            m2[s[j]-'a']++;
+            if((j-i+1)< n){
                 j++;
             }
-            else if((j-i+1)==lenofp){
-                if(vecs==vecp){
-                    res.push_back(i);
+            else if((j-i+1)==n){
+                if(m1==m2){
+                    ans.push_back(i);
                 }
-                vecs[s[i]-'a']--;
+                m2[s[i]-'a']--;
                 i++;
                 j++;
             }
         }
-        return res;
+        return ans;
     }
 };
