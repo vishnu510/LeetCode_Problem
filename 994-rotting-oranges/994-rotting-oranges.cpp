053c2a07ceb0426d5c396dde7fb70b6776a9fdef
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-08-13 16:21:14 +0530

    Time: 24 ms (9.38%), Space: 13.1 MB (78.80%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,41 +1,64 @@
+struct node{
+    int x,y,time;
+    
+    node(int _x,int _y,int _time){
+        
+        x = _x;
+        y = _y;
+        time = _time;
+    }
+};
+
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-       int row = grid.size();
-       int col = grid[0].size();
-       vector<int> direction = {-1,0,1,0,-1};
-       queue<pair<int,int>> que;
-       int freshOrange = 0;
-       for(int i=0;i<row;i++){
-           for(int j=0;j<col;j++){
-               if(grid[i][j]==2){
-                   que.push({i,j});
-               }
-               if(grid[i][j]==1){
-                   freshOrange++;
-               }
-           }
-       }
-       int timeTaken = -1;
-        while(!que.empty()){
-            int queSize = que.size();
-            while(queSize--){
-                pair<int,int> pair = que.front();
-                que.pop();
-                for(int i=0;i<4;i++){
-                    int r = pair.first + direction[i];
-                    int c = pair.second + direction[i+1];
-                    if(r>=0 && r<row && c>=0 && c<col && grid[r][c]==1){
-                        grid[r][c]=2;
-                        que.push({r,c});
-                        freshOrange--;
-                    }
+       
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        queue<node> q;
+        int countOrange =0;
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<m;j++){
+                
+                if(grid[i][j]==2){
+                    q.push(node(i,j,0));
+                }
+                if(grid[i][j]!=0){
+                    countOrange++;
                 }
             }
-            timeTaken++;
         }
-        if(freshOrange>0) return -1;
-        if(timeTaken == -1) return 0;
-        return timeTaken;
+        
+        int ans =0,count =0;
+        
+        while(!q.empty()){
+            
+            int x = q.front().x;
+            int y = q.front().y;
+            int time  = q.front().time;
+            
+            q.pop();
+            
+            ans = max(ans,time);
+            count++;
+            
+            int dx[] = {-1,0,1,0};
+            int dy[] = {0,1,0,-1};
+            
+            for(int i=0;i<4;i++){
+                
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                
+                if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY]==1){
+                    grid[newX][newY] = 2;
+                    q.push(node(newX,newY,time+1));
+                }
+            }
+        }
+        if(count==countOrange) return ans;
+        return -1;
     }
 };
