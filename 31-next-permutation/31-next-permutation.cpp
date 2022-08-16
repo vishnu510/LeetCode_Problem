b8a351f9b9f7d3e95a576fa99411fcbf9e0e67c7
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-08-16 16:55:27 +0530

    Time: 11 ms (26.40%), Space: 12.1 MB (36.78%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        int breakPoint =-1;
+        int breakingPoint = -1;
         
-        for(int i =nums.size()-1;i>0;i--){
+        for(int i = nums.size()-1; i >0; i--){
             
-            if(nums[i]>nums[i-1]){
-                breakPoint = i-1;
-                break;
-            }
-        }
-        if (breakPoint < 0)
-    {
-        reverse(nums.begin(), nums.end());
-        return;
-    }
-        for(int i =nums.size()-1;i>=0;i--){
-            
-            if(nums[i]>nums[breakPoint]){
-                swap(nums[i],nums[breakPoint]);
-                reverse(nums.begin()+breakPoint+1,nums.end());
+            if(nums[i] > nums[i-1]){
+                breakingPoint = i-1;
                 break;
             }
         }
         
+        if(breakingPoint < 0){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
         
+        for(int i = nums.size()-1; i>=0; i--){
+            
+            if(nums[i] > nums[breakingPoint]){
+            
+            swap(nums[i],nums[breakingPoint]);
+            
+            reverse(nums.begin() + breakingPoint + 1, nums.end());
+            
+            break;
+            }
+        }
     }
 };
