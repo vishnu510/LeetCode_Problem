d6f64b3098906ac3c3a312c371000e65772f5295
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-08-23 16:30:16 +0530

    Time: 477 ms (18.93%), Space: 132.6 MB (5.06%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,26 +11,19 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-         stack<int> st;
-   bool flag = true;
-    ListNode *temp1 = head;
-   while (temp1!=NULL)
-   {   
-      
-       st.push(temp1->val);
-        temp1 = temp1->next;
-   }
-   while (head!=NULL)
-   {   
-       
-       if(st.top()!=head->val){
-           flag = false;
-       }
-       head = head->next;
-       st.pop();
-   }
- 
-   return flag;
+        vector<int> num;
         
+        ListNode* curr = head;
+        
+        while(curr){
+            num.push_back(curr->val);
+            curr = curr->next;
+        }
+        
+        vector<int> copy = num;
+        
+        reverse(copy.begin(),copy.end());
+        
+        return num == copy;
     }
 };
