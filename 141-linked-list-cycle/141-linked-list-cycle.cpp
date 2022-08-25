d9d2eb0fc2bd6eaa132f35cc875f623d1c1a1d6e
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-08-25 15:08:28 +0530

    Time: 19 ms (41.13%), Space: 8.1 MB (17.52%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,15 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL){
-            return false;
-        }
-        ListNode * slow = head;
-        ListNode * fast = head;
+        if(head==NULL) return false;
+        
+        ListNode *slow  = head;
+        ListNode *fast = head;
+        
         while(fast->next && fast->next->next){
             slow = slow->next;
-            fast = fast->next->next;
-            if(slow==fast){
+            fast =fast->next->next;
+            if(slow == fast){
                 return true;
             }
         }
