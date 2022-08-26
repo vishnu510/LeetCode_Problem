1aba511d85c0d26395d6114862dc5bcd455e7b4d
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-08-27 02:45:46 +0530

    Time: 215 ms (29.72%), Space: 23.5 MB (27.81%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-   sort(nums.begin() , nums.end());  
-        if(nums.size() < 3){   
-            return {};
-        }
-        if(nums[0] > 0){      
-            return {};
-        }
-        vector<vector<int>> answer;
-        for(int i = 0 ; i < nums.size() ; ++i){   
-            if(nums[i] > 0){    
-                break;
-            }
-            if(i > 0 && nums[i] == nums[i - 1]){   
-                continue;
-            }
-            int low = i + 1 , high = nums.size() - 1; 
-            int sum = 0;
-            while(low < high){          
-                sum = nums[i] + nums[low] + nums[high];
-                if(sum > 0){  
-                    high--;
-                } else if(sum < 0){ 
-                    low++;
-                } else {
-                    answer.push_back({nums[i] , nums[low] , nums[high]}); 
-                    int last_low_occurence = nums[low] , last_high_occurence = nums[high]; 
-                    while(low < high && nums[low] == last_low_occurence){  
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<nums.size()-2;i++){
+            
+            if(i==0 || (i>0 && nums[i] != nums[i-1])){
+                
+                int low = i+1, high = nums.size()-1,sum = 0-nums[i];
+                
+                while(low < high){
+                    if(nums[low]+nums[high]==sum){
+                        vector<int> temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[low]);
+                        temp.push_back(nums[high]);
+                        ans.push_back(temp);
+                        while( low <high && nums[low] == nums[low+1]) low++;
+                        while(low <high && nums[high] == nums[high-1]) high--;
                         low++;
-                    }
-                    while(low < high && nums[high] == last_high_occurence){
                         high--;
                     }
+                    else if(nums[low]+nums[high] < sum) low++;
+                    else high--;
                 }
             }
         }
-        return answer;   
-        
+        return ans;
     }
 };
