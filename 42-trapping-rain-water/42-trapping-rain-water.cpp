4280c55a942a6a10a8e9bc46854f75a328d70ffa
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-08-27 03:32:56 +0530

    Time: 41 ms (9.18%), Space: 20.7 MB (11.55%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-     int size = height.size();
-    vector<int> left(size,0);
-    vector<int> right(size,0);
-    int answer=0,l=height[0],m=height[size-1];
-    for (int i = 0; i < size; i++)
-    {
-        left[i] = max(l,height[i]);
-        l = max(l,height[i]);
-    }
-    for (int i = size-1; i >=0 ; i--)
-    {
-        right[i] = max(m,height[i]);
-        m = max(m,height[i]);
-    }
-    for (int i = 0; i < size; i++)
-    {
-        answer+= min(left[i],right[i])-height[i];
-    }
-        return answer;
+        int n = height.size();
+        vector<int> prefix(n,0),suffics(n,0);
+        int mx = height[0];
+        for(int i =0;i<n;i++){
+            mx = max(mx,height[i]);
+            prefix[i] = mx;
+        }
+        int mx2 = height[height.size()-1];
+        for(int i = n-1;i>=0;i--){
+            mx2 = max(mx2,height[i]);
+            suffics[i] = mx2;
+        }
+        
+        int ans =0;
+        for(int i=0;i<height.size();i++){
+            ans += min(prefix[i],suffics[i])-height[i];
+        }
+        return ans;
     }
 };
