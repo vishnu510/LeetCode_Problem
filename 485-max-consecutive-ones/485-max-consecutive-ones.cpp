6db30fbef6719cbb41e7c495faff0dd4ea558983
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-08-27 22:50:40 +0530

    Time: 71 ms (32.93%), Space: 36.3 MB (22.17%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -7,11 +7,11 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i] == 1){
                 count++;
-                mx = max(mx,count);
             }
             else if( nums[i] ==0){
                 count = 0;
             }
+            mx = max(mx,count);
         }
         return mx;
     }
