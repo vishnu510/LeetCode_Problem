44dbb00bc8147b182dbdd31fce0541730bbc6d2a
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-09-14 16:24:56 +0530

    Time: 52 ms (15.04%), Space: 15.3 MB (16.38%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,54 +1,44 @@
 class Solution {
 public:
-    
     class Trie{
         public:
-        
         unordered_map<char,Trie*> children;
-        bool isEnd  = false;
+        bool isEnd = false;
+        void insert(string str){
+        Trie* node = this;
         
-        void insert(string word){
-            
-            Trie* node = this;
-            
-            for(char c:word){
-                
-                if(node->children.find(c)==node->children.end()){
-                    node->children[c] = new Trie();
-                }
-                
-                node = node->children[c];
+        for(auto c:str){
+            if(node->children.find(c) == node->children.end()){
+                node->children[c] = new Trie();
             }
-            
-            node->isEnd = true;
+            node = node->children[c];
         }
+        node->isEnd = true;
+    }
+    bool search(string str){
+        Trie* node = this;
         
-        bool search(string word){
-            
-            Trie* node = this;
-            for(char c:word){
-                if(node->children.find(c)==node->children.end()){
+        for(auto c:str){
+            if(node->children.find(c) == node->children.end()){
                 return false;
             }
-                node = node->children[c];
-            }
-            return node->isEnd;
+            node = node->children[c];
         }
-        
+        return node->isEnd;
+    }
     };
     
+    
     bool wordBreak(string s, vector<string>& wordDict) {
-        
         Trie trie;
-        
-        for(auto word:wordDict){
-            trie.insert(word);
+        for(auto str:wordDict){
+            trie.insert(str);
         }
-         vector<bool> dp(s.size()+1,false);
+        vector<bool> dp(s.size()+1,false);
         dp[0] = true;
+        
         for(int i=1;i<=s.size();i++){
-            for(int j=0;j<i;j++){
-                
+            for(int j =0;j<i;j++){
                 if(dp[j] && trie.search(s.substr(j,i-j))){
                     dp[i] = true;
                 }
