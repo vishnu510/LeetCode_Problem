93352ed35a153c20c2660a8f752f9bc6d2f994b4
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-09-14 17:18:45 +0530

    Time: 3 ms (51.66%), Space: 7.3 MB (37.36%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,57 +1,56 @@
 class Solution {
 public:
     vector<string> ans;
+    
     class Trie{
         public:
         Trie* child[26];
         bool isEnd = false;
-        
     };
     
-    void insert(string & word,Trie* root){
-        Trie* curr = root;
-        for(char c:word){
-            if(curr->child[c-'a']==nullptr){
-                curr->child[c-'a'] = new Trie();
-            }
-            curr = curr->child[c-'a'];
-        }
-        curr->isEnd = true;
-    }
-    
-    bool search(string & word,Trie* root){
-        Trie* curr = root;
+    void insert(string & str,Trie* root){
         
-        for(char c:word){
-            
-            if(curr->child[c-'a']==nullptr) return false;
-            curr = curr->child[c-'a'];
+        Trie* node = root;
+        for(auto c:str){
+            if(node->child[c-'a'] == nullptr){
+                node->child[c-'a'] = new Trie();
+            }
+            node = node->child[c-'a'];
         }
-        return curr->isEnd;
+        node->isEnd = true;
     }
-    
-    void dfs(string &s,int currIdx, Trie* root,string currStr){
-        if(currIdx==s.size()){
+    bool search(string & str,Trie* root){
+        
+        Trie* node = root;
+        for(auto c:str){
+            if(node->child[c-'a'] == nullptr){
+                return false;
+            }
+            node = node->child[c-'a'];
+        }
+        return node->isEnd;
+    }
+    void helper(string & s,int currIdx,Trie* root,string currStr){
+        if(currIdx == s.size()){
             ans.push_back(currStr.substr(0,0+currStr.size()-1));
             return;
         }
-        string res ="";
-        for(int i=currIdx;i<s.size();++i){
+        string res = "";
+        for(int i=currIdx;i<s.size();i++){
             res.push_back(s[i]);
             if(search(res,root)){
-                dfs(s,i+1,root,currStr+res+" ");
+                helper(s,i+1,root,currStr+res+" ");
             }
         }
     }
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        
         Trie* root = new Trie();
         
-        for(auto word:wordDict){
-            insert(word,root);
+        for(auto str:wordDict){
+            insert(str,root);
         }
-        dfs(s,0,root,"");
+        helper(s,0,root,"");
         return ans;
     }
 };
