fffeccf3195f22cd64a302d59e893f278488c886
Vishnu Pal Dubey <vpal6044@gmail.com>
2022-09-22 18:27:40 +0530

    Time: 46 ms (17.76%), Space: 9.8 MB (45.42%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int start = 0,end=0;
+        int start =0,end =0;
+        
         for(int i=0;i<s.size();i++){
             if(s[i]==' '){
-                end = i;
+                end =i;
                 reverse(s.begin()+start,s.begin()+end);
-                start = i+1;
+                start = end+1;
             }
         }
         reverse(s.begin()+start,s.end());
